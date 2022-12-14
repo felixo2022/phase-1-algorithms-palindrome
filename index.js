86e42f0247dab0132eb97f8af3a20682090ee6ab
felixo2022 <walterbarax@gmail.com>
2022-12-14 05:54:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word.length < 2) return true
+
+
+  let firstWord = 0
+  let lastWord = word.length - 1
+
+  while(firstWord <= lastWord){
+    if(word[firstWord] != word[lastWord]){
+      return false
+    }
+    firstWord++
+    lastWord--
+  }
+
+    return true
 }
 
 /* 
-  Add your pseudocode here
+  1. find word with less than 2 letters
+  2. return false if lastWord is not firstWord
+  3. increment firstWord and decrement lastWord
+  4. return true if firstWord matches lastWord
 */
 
 /*
-  Add written explanation of your solution here
+palindrome words are characters that read the same backward as forward hereby, we need to return true if the word is less than 2 letters, i achieve
+that by looping through the string using the while loop.  
 */
 
 // You can run `node index.js` to view these console logs
